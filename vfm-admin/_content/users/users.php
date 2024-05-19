7c1d799e26efadc6e863f11c03b44ca30733d5c7
achmadbaihaqi <achmadbaihaqi@gmail.com>
2024-05-19 14:25:23 +0700

    Initiate

---
+++ vfm-admin/_content/users/users.php
@@ -0,0 +1,18 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$8wMVhVIk$c8q7SBlJYT.pFinww.bgw/',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'ahsan',
+    'pass' => '$1$YsWAZ7ID$qsz1YlfT6jD4jNk9GU3xA1',
+    'role' => 'user',
+    'disabled' => false,
+    'email' => 'achsan@gmail.com',
+  ),
+);
